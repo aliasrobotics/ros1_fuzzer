d2604811e47d9d318e7a475edf18096bd335b41a
Gorka Olalde <gorka.olalde@gmail.com>
2019-05-13 16:54:42 +0200

    Add basic unit tests as example of usage, as well as for testing the correct behaviour of the fuzzer

---
+++ test.py
@@ -0,0 +1,35 @@
+import unittest
+
+import rospy
+from hypothesis import given
+from rosgraph_msgs.msg import Log
+from std_msgs.msg import Header
+from rosgraph_msgs.msg import Log
+from ros_commons import map_ros_types, ros_msg_loader_str
+from hypothesis.strategies import lists, text
+
+class TestRosLogMessages(unittest.TestCase):
+
+    def setUp(self):
+        self.pub = rospy.Publisher('/rosout', Log, queue_size=10)
+        self.node = rospy.init_node('fuzzer_node', anonymous=False)
+
+    def tearDown(self):
+        rospy.signal_shutdown('Shutting down fuzzer node')
+
+    @given(header=map_ros_types(Header))
+    def test_fuzz_log_message(self, header):
+        log = Log()
+        log.header = header
+        self.pub.publish(log)
+
+    @given(log=map_ros_types(Log))
+    def test_fuzz_log_message_exclude(self, log):
+        log.name = 'Fixed name'
+        self.pub.publish(log)
+
+    @given(log=map_ros_types(Log), name=text(min_size=10, max_size=20))
+    def test_fuzz_log_message_arbitrary(self, log, name):
+        log.name = name
+        self.pub.publish(log)
+
