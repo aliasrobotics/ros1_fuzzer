30fec7c21355699a7546721e4a03e26edcb77445
Borken <erice.borja@hotmail.com>
2019-05-14 09:37:06 +0200

    added setup.py, reordered folder structure

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='ros1_fuzzer',
+    version='1.0',
+    packages=['ros1_fuzzer'],
+    url='https://www.aliasrobotics.com',
+    license='GPLv2',
+    author='Alias Robotics',
+    author_email='contact@aliasrobotics.com',
+    description='A ROS subscriber fuzzing tool for ROS systems',
+    keywords=['network', 'fuzzing', 'ros'],
+    entry_points={
+        'console_scripts': ['ros1_fuzzer=ros1_fuzzer.ros_fuzzer:main'],
+    },
+    install_requires=[
+        'hypothesis==3.82',
+        'attrs==19.1.0',
+        'enum34==1.1.6',
+    ],
+    include_package_data=True,
+)
