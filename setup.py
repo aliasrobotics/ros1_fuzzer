1e4291b124c97309d49887f24ccc80e3d78f3f40
Gorka Olalde <gorka.olalde@gmail.com>
2019-05-16 16:23:53 +0200

    Add ability to install as ROS package

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='ros1_fuzzer',
-    version='1.0',
+    version='1.0.0',
     packages=['ros1_fuzzer'],
     url='https://www.aliasrobotics.com',
     license='GPLv2',
@@ -18,6 +18,9 @@ setup(
         'attrs==19.1.0',
         'enum34==1.1.6',
         'psutil==5.6.2'
+    ],
+    tests_requires=[
+        'pytest==4.5.0',
     ],
     include_package_data=True,
 )
